86bf83080a363a0279010782c220265712ceae49
freirediego23 <diegofg23@outlook.es>
2020-11-20 17:57:29 -0500

    added

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ express()
 
         // Log this to the console for debugging purposes.
         console.log("Back from DB with result:");
-        var a = res.send(JSON.stringify(result.columns));
-        document.getElementById('list').innerHTML = a;
+        res.send(JSON.stringify(result.columns));
+        
 
     });
 
